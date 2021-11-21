1d0ce2120304716504effecab3af2fcc2825fc08
I-LOVE-BASH <93775080+I-LOVE-BASH@users.noreply.github.com>
2021-11-21 09:58:36 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import  os
+os.system("clear && figlet MATH")
+# Regular Colors  
+Black="\[\033[0;30m\]" # Black
+Red="\[\033[0;31m\]" # Red
+Green="\[\033[0;32m\]" # Green
+Yellow="\[\033[0;33m\]" # Yellow
+Blue="\[\033[0;34m\]" # Blue
+Purple="\[\033[0;35m\]" # Purple
+Cyan="\[\033[0;36m\]" # Cyan
+White="\[\033[0;37m\]" # White
+# High Intensty
+IBlack="\[\033[0;90m\]" # Black
+IRed="\[\033[0;91m\]" # Red
+IGreen="\[\033[0;92m\]" # Green
+IYellow="\[\033[0;93m\]" # Yellow
+IBlue="\[\033[0;94m\]" # Blue
+IPurple="\[\033[0;95m\]" # Purple
+ICyan="\[\033[0;96m\]" # Cyan
+IWhite="\[\033[0;97m\]" # White
+# Bold High Intensty
+BIBlack="\[\033[1;90m\]" # Black
+BIRed="\[\033[1;91m\]" # Red
+BIGreen="\[\033[1;92m\]" # Green
+BIYellow="\[\033[1;93m\]" # Yellow
+BIBlue="\[\033[1;94m\]" # Blue
+BIPurple="\[\033[1;95m\]" # Purple
+BICyan="\[\033[1;96m\]" # Cyan
+BIWhite="\[\033[1;97m\]" # White
+print("""
+\033[1;96m1)\033[1;94m|x+a|=c\033[1;97m
+\033[1;96m2)\033[1;94m|x-a|=c\033[1;97m
+\033[1;96m3)\033[1;94m|x*a|=c\033[1;97m
+\033[1;96m4)\033[1;94m|x/a|=c\033[1;97m
+\033[1;96m5)\033[1;94m|x-a|*|x+b|=c\033[1;97m
+\033[1;96m6)\033[1;94m|x-a|/|x+b|=c\033[1;97m
+\033[1;96m7)\033[1;94m|x+a|-|x-b|=c\033[1;97m
+\033[1;96m8)\033[1;94m|x+a|+|x-b|=c\033[1;97m
+""")
+r=input("\033[1;92m==>\033[1;91m ")
+if r == "1" :
+    a=int(input("A \033[1;92m==>\033[1;91m "))
+    c=int(input("C \033[1;92m==>\033[1;91m "))
+    print("\033[0;92m| \033[0;94mx \033[0;92m+\033[0;94m",a,"\033[0;92m| =\033[0;94m",c)
+    print("\033[0;92m( \033[0;94mx \033[0;92m+\033[0;94m",a,"\033[0;92m) =\033[0;94m",c,"or \033[0;92m-( \033[0;94mx \033[0;92m+\033[0;94m",a,"\033[0;92m) =\033[0;94m",c)
+    x=c-a
+    x2=-c-a
+    print("\033[0;94mX1 \033[0;92m=\033[0;94m",x)
+    print("\033[0;94mX2 \033[0;92m=\033[0;94m",x2)
+if r == "2" :
+    a=int(input("A \033[1;92m==>\033[1;91m "))
+    c=int(input("C \033[1;92m==>\033[1;91m "))
+    print("|x-a|=c")
+if r == "3" :
+    a=int(input("A \033[1;92m==>\033[1;91m "))
+    c=int(input("C \033[1;92m==>\033[1;91m "))
+    print("\033[0;92m| \033[0;94x \033[0;92m* \033[0;94a \033[0;92m| = \033[0;94c")
